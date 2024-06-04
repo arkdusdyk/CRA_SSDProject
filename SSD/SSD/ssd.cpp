da1048952d0a493440b4df49cb7002336f3289cb
Jaewon Kim <jwn0222@gmail.com>
2024-06-04 15:47:52 +0900

    implement read() and write() method
    Signed-off-by: Jaewon Kim <jwn0222@gmail.com>

--- SSD/SSD/ssd.cpp
+++ SSD/SSD/ssd.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
+#include <sstream>
+#include <stdexcept>
 
 using namespace std;
 
@@ -39,12 +42,73 @@ public:
 	}
 
 	void write(int address, int data) {
+		string ssdData[100];
 
+		if (address < 0 || address >= 100)
+			throw out_of_range("address range is 0 < address < 100");
+
+		/* First write case */
+		ifstream checkFile(NAND);
+		if (!checkFile.good()) {
+			ofstream firstFile(NAND);
+			for (int i = 0; i < 100; i++) {
+				firstFile << "0" << endl;
+			}
+			firstFile.close();
+		}
+		checkFile.close();
+
+		/* Get data */
+		ifstream inFile(NAND);
+		if (inFile.is_open()) {
+			string line;
+			int cnt = 0;
+			string readData;
+			while (getline(inFile, line)) {
+				ssdData[cnt++] = line;
+			}
+		}
+		inFile.close();
+
+		/* Write data */
+		stringstream dataToHex;
+		dataToHex << std::hex << data;
+		ssdData[address] = dataToHex.str();
+		ofstream outFile(NAND);
+		if (outFile.is_open()) {
+			for (int i = 0; i < 100; i++) {
+				outFile << ssdData[i] << endl;
+			}
+		}
+		outFile.close();
 	}
 
 	int read(int address) {
+		string ssdData[100];
 		int data = 0;
-		return data;
+
+		if (address < 0 || address >= 100)
+			throw out_of_range("address range is 0 < address < 100");
+
+		ifstream inFile(NAND);
+		if (inFile.is_open()) {
+			string line;
+			int cnt = 0;
+			string readData;
+			while (getline(inFile, line)) {
+				ssdData[cnt++] = line;
+			}
+		}
+		inFile.close();
+
+		ofstream outFile(OUTPUT);
+		if (outFile.is_open()) {
+			outFile << ssdData[address] << endl;
+
+		}
+		outFile.close();
+
+		return 0;
 	}
 
 private:
