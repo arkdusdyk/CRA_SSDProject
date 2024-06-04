7b8e89a30b0c62ca815e35c3c8f0246fe77caecd
bsk <lightjd@naver.com>
2024-06-04 14:32:07 +0900

    Added unittest for ssd commandChecker

--- SSD/Sample-Test1/test.cpp
+++ SSD/Sample-Test1/test.cpp
@@ -47,9 +47,60 @@ TEST_F(SSDFIxture, SsdRead99) {
 TEST_F(SSDFIxture, SsdRead100) {
     int address = 100;
     int expected = 0x00000000;
-    int result = ssd.read(address);
     EXPECT_THROW({
          ssd.read(address);
         }, std::out_of_range);
 }
 
+TEST_F(SSDFIxture, SsdCommandCheckParamWrite0) {
+    int argc = 4;
+    char* argv[] = { "ssd.exe", "W", "0", "0xdeadbeef"};
+    CommandSet cmd;
+    int result = ssd.checkParameter(argc, argv, cmd);
+
+    int expectedAddress = 0;
+    int expectedData = 0xdeadbeef;
+    EXPECT_THAT(cmd.cmdOpcode, testing::Eq(SSD::COMMAND_WRITE));
+    EXPECT_THAT(cmd.address, testing::Eq(expectedAddress));
+    EXPECT_THAT(cmd.data, testing::Eq(0xdeadbeef));
+    EXPECT_THAT(result, testing::Eq(SSD::COMMAND_VALIDATION_SUCCESS));
+}
+
+TEST_F(SSDFIxture, SsdCommandCheckParamRead0) {
+    int argc = 3;
+    char* argv[] = { "ssd.exe", "R", "0"};
+    CommandSet cmd;
+    int result = ssd.checkParameter(argc, argv, cmd);
+
+    int expectedAddress = 0;
+    EXPECT_THAT(cmd.cmdOpcode, testing::Eq(SSD::COMMAND_READ));
+    EXPECT_THAT(cmd.address, testing::Eq(expectedAddress));
+    EXPECT_THAT(result, testing::Eq(SSD::COMMAND_VALIDATION_SUCCESS));
+}
+
+
+TEST_F(SSDFIxture, SsdCommandCheckParamWrite100) {
+    int argc = 4;
+    char* argv[] = { "ssd.exe", "W", "100", "0xdeadbeef" };
+    CommandSet cmd;
+    int result = ssd.checkParameter(argc, argv, cmd);
+
+    int expectedAddress = 100;
+    int expectedData = 0xdeadbeef;
+    EXPECT_THAT(cmd.cmdOpcode, testing::Eq(SSD::COMMAND_WRITE));
+    EXPECT_THAT(cmd.address, testing::Eq(expectedAddress));
+    EXPECT_THAT(cmd.data, testing::Eq(0xdeadbeef));
+    EXPECT_THAT(result, testing::Eq(SSD::COMMAND_VALIDATION_SUCCESS));
+}
+
+TEST_F(SSDFIxture, SsdCommandCheckParamRead100) {
+    int argc = 3;
+    char* argv[] = { "ssd.exe", "R", "100" };
+    CommandSet cmd;
+    int result = ssd.checkParameter(argc, argv, cmd);
+
+    int expectedAddress = 100;
+    EXPECT_THAT(cmd.cmdOpcode, testing::Eq(SSD::COMMAND_READ));
+    EXPECT_THAT(cmd.address, testing::Eq(expectedAddress));
+    EXPECT_THAT(result, testing::Eq(SSD::COMMAND_VALIDATION_SUCCESS));
+}
