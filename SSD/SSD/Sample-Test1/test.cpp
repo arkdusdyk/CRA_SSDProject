4ef96355b82295fa81033c16169e1b94fc200967
bsk <lightjd@naver.com>
2024-06-04 12:12:19 +0900

    Added Initial Unittest for ssd

--- SSD/SSD/Sample-Test1/test.cpp
+++ SSD/SSD/Sample-Test1/test.cpp
@@ -1,7 +1,53 @@
+#include <stdexcept>
+
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+class SSDFIxture : public testing::Test {
+public:
+    SSD ssd;
+};
+
+TEST_F(SSDFIxture, SsdWrite0) {
+    int address = 0;
+    int data = 0xdeadbeef;
+    ssd.write(address, data);
 }
+
+TEST_F(SSDFIxture, SsdWrite99) {
+    int address = 0;
+    int data = 0xdeadbeef;
+    ssd.write(address, data);
+}
+
+TEST_F(SSDFIxture, SsdWrite100) {
+    int address = 100;
+    int data = 0xdeadbeef;
+    EXPECT_THROW({
+         ssd.write(address, data);
+        }, std::out_of_range);
+}
+
+TEST_F(SSDFIxture, SsdRead0) {
+    int address = 0;
+    int expected = 0x00000000;
+    int result = ssd.read(address);
+    EXPECT_THAT(data, testing::Eq(expected));
+}
+
+TEST_F(SSDFIxture, SsdRead99) {
+    int address = 99;
+    int expected = 0x00000000;
+    int result = ssd.read(address);
+    EXPECT_THAT(data, testing::Eq(expected));
+}
+
+TEST_F(SSDFIxture, SsdRead100) {
+    int address = 100;
+    int expected = 0x00000000;
+    int result = ssd.read(address);
+    EXPECT_THROW({
+         ssd.read(address);
+        }, std::out_of_range);
+}
+
