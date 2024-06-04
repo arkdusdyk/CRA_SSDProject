5fbcffda3441aed2170cbbff0eba7050fb592662
kwonluck <software_k@naver.com>
2024-06-04 12:00:06 +0900

    [FEAT] TestShell TestCase 작성
    1. 기본 TC 작성
    2. TestShell 기초 class 생성

---
+++ TestShell/TestShell/TestShell.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class TestShell {
+public:
+	void Write(int arr, string value) {
+
+	}
+	string Read(int arr) {
+
+	}
+
+	void FulllWrite( string value) {
+
+	}
+
+	string FullRead(string value) {
+
+	}
+};
