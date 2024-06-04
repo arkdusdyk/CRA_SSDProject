df46a1eff73ed948bc746b4b59b9a87f4484e310
yoyo <arkdusdyk@gmail.com>
2024-06-04 11:13:51 +0900

    Initial Commit - Project Config

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,10 @@
+#include <stdexcept>
+
+#include "../CRA_SSDProject/SSD.cpp"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+TEST(TestCaseName, TestName) {
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
