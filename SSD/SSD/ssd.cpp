583498842cf8ced81aa105c3335de3c4df5c60fb
bsk <lightjd@naver.com>
2024-06-04 15:14:42 +0900

    Changed SSD checkParameter argument

--- SSD/SSD/ssd.cpp
+++ SSD/SSD/ssd.cpp
@@ -4,9 +4,22 @@
 
 using namespace std;
 
+struct CommandSet
+{
+	int cmdOpcode;
+	int address;
+	int data;
+};
+
 class SSD {
 public:
-	int checkParameter(char* args, int& command, int& address, int& data) {
+	static const int COMMAND_VALIDATION_SUCCESS = 0;
+	static const int COMMAND_VALIDATION_FAIL = -1;
+
+	static const int COMMAND_WRITE = 0x1;
+	static const int COMMAND_READ = 0x2;
+
+	int checkParameter(int argc, char* argv[], CommandSet& commandset) {
 		return 0;
 	}
 
@@ -18,6 +31,7 @@ public:
 		int data = 0;
 		return data;
 	}
+
 private:
 	const string OUTPUT = "result.txt";
 	const string NAND = "nand.txt";
