00d2410b8ac1d7cda8b5170319bbf6ad60770050
bsk <lightjd@naver.com>
2024-06-04 16:16:49 +0900

    Implemented application code of SSD
    Plus, fixed wrong execution file name from 'ssd.exe.exe' to 'ssd.exe'

--- SSD/SSD/main.cpp
+++ SSD/SSD/main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include "ssd.cpp"
 
 int main(int argc, char* argv[])
 {
-    std::cout << argc << std::endl;
-    for (int i = 0; i < argc; i++)
+    SSD ssd;
+    CommandSet cmd;
+    int ret = ssd.checkParameter(argc, argv, cmd);
+    if (ret == SSD::COMMAND_VALIDATION_FAIL)
+        return -1;
+
+    if (cmd.cmdOpcode == SSD::COMMAND_WRITE)
     {
-        std::cout << argv[i] << std::endl;
+        ssd.write(cmd.address, cmd.data);
     }
+    else if (cmd.cmdOpcode == SSD::COMMAND_READ)
+    {
+        ssd.read(cmd.address);
+    }
+    else
+        return -1;
+
     return 0;
 }
