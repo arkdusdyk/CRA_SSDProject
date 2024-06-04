62b4c1c262ca235cf48b5cd74a6bccd1c80d2333
DESKTOP-5FBE16T\User <yjhl2003@gmail.com>
2024-06-04 14:33:32 +0900

    test shell read 유닛테스트 추가

--- TestShell/Sample-Test1/test.cpp
+++ TestShell/Sample-Test1/test.cpp
@@ -5,17 +5,41 @@
 #include "gmock/gmock.h"
 
 using namespace testing;
+using namespace std;
 
 
-class Mock : public TestShell {
+class MockTestShell : public TestShell {
 public:
-	MOCK_METHOD(void, Write, (int addr, string value), (override));
-	MOCK_METHOD(string, Read, (int addr), (override));
-	MOCK_METHOD(void, FullWrite, (string value), (override));
-	MOCK_METHOD(string, FullRead, , (override));
+	MOCK_METHOD(void, Write, (int addr, string value), ());
+	MOCK_METHOD(string, Read, (int addr), ());
+	MOCK_METHOD(void, FullWrite, (string value), ());
+	MOCK_METHOD(string, FullRead, (), ());
 };
-
 
 TEST(TestCaseName, TestName) {
 	EXPECT_THAT(1, Eq(1));
+}
+
+TEST(ShellReadTest, ReadTest) {
+	MockTestShell mock;
+
+	EXPECT_CALL(mock, Read(0)).WillOnce(Return("Ox00000000"));
+
+	cout << mock.Read(0) << "\n";
+}
+
+TEST(ShellReadTest, ReadFailTest) {
+	MockTestShell mock;
+
+	EXPECT_THROW(mock.Read(110), exception);
+
+}
+
+TEST(ShellReadTest, FullReadTest) {
+	MockTestShell mock;
+
+	EXPECT_CALL(mock, FullRead()).Times(1);
+	EXPECT_CALL(mock, Read(_)).Times(100);
+
+	cout << mock.FullRead() << "\n";
 }
