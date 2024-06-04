c82e21586ea2a20937c7b6b930f2db51b3cb83bf
suyounglee <diony_sos@outlook.com>
2024-06-04 16:24:07 +0900

    SSD app : update SSDProtocol.
    Signed-off-by: suyounglee <diony_sos@outlook.com>

--- TestShell/TestShell/TestShell.cpp
+++ TestShell/TestShell/TestShell.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using namespace std;
@@ -13,12 +14,29 @@ class SSDProtocol : public IProtocol
 {
 public:
 	virtual void Write(int addr, string value) override {
-
+		string cmd;
+		cmd.append(mExecuteName + " W " + std::to_string(addr) + " " + value);
+		system(cmd.c_str());
 	}
 
 	virtual string Read(int addr) override {
-		return "";
+		string cmd;
+		cmd.append(mExecuteName + " R " + std::to_string(addr));
+		system(cmd.c_str());
+		ifstream readFile;
+		string result;
+		readFile.open(mReadFileName);
+		if (readFile.is_open()) {
+			string buf;
+			while (getline(readFile, buf)) {
+				result.append(buf);
+			}
+		}
+		return result;
 	}
+private:
+	string mReadFileName = "result.txt";
+	string mExecuteName = "ssd.exe";
 };
 
 class TestShell {
