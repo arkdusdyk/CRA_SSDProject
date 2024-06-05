f55356e494ffef44979cb30098bb2f2ca9241418
yoyo <arkdusdyk@gmail.com>
2024-06-05 13:51:00 +0900

    Parser add features, call from main

--- TestShell/TestShell/CommandParser.cpp
+++ TestShell/TestShell/CommandParser.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
@@ -24,33 +25,58 @@ public:
 		}
 		tokens.push_back(cmd_line.substr(prev, cur - prev));
 
-		if (tokens.size() == 1)				//	exit, help, fullwrite, fullread
+		if (tokens.size() == 1) {			//	exit, help, fullread
 			cmd = tokens[0];
-		else if (tokens.size() == 2) {		// read LBA
+			if ((cmd == "exit") || (cmd == "help") || (cmd == "fullread")) {
+				//cout << "cmd : " << cmd << "\n";
+			}
+			else
+				throw exception("INVALID COMMAND");
+		}
+		else if (tokens.size() == 2) {		// read LBA, fullwrite data
 			cmd = tokens[0];
-			if (isLBAint(tokens[1]) == false)
-				throw exception("LBA out of range");
-			if (isLBAinRange(tokens[1]) == false)
-				throw exception("LBA out of range");
-			lba = stoi(tokens[1]);
+			if (cmd == "read") {
+				if (isLBAint(tokens[1]) == false)
+					throw exception("LBA out of range");
+				if (isLBAinRange(tokens[1]) == false)
+					throw exception("LBA out of range");
+				lba = stoi(tokens[1]);
+				//cout << "cmd : " << cmd << " lba : " << lba << "\n";
+			}
+			else if (cmd == "fullwrite") {
+				if (isDataValid(tokens[2]) == false)
+					throw exception("Data out of range");
+				data = tokens[2];
+				//cout << "cmd : " << cmd << " data : " << lba << "\n";
+			}
+			else
+				throw exception("INVALID COMMAND");
 		}
 		else if (tokens.size() == 3) {		// write LBA data
 			cmd = tokens[0];
-			if (isLBAint(tokens[1]) == false)
-				throw exception("LBA out of range");
-			if (isLBAinRange(tokens[1]) == false)
-				throw exception("LBA out of range");
-			lba = stoi(tokens[1]);
+			if (cmd == "write") {
+				if (isLBAint(tokens[1]) == false)
+					throw exception("LBA out of range");
+				if (isLBAinRange(tokens[1]) == false)
+					throw exception("LBA out of range");
+				lba = stoi(tokens[1]);
+				if (isDataValid(tokens[2]) == false)
+					throw exception("Data out of range");
+				data = tokens[2];
+				//cout << "cmd : " << cmd << " lba : " << lba << " data : " << data << "\n";
+			}
+			else
+				throw exception("INVALID COMMAND");
 		}
 		else if(tokens.size() > 3)
 			throw exception("INVALID COMMAND");
 	}
 
 	bool isLBAint(string LBA_string) {
-		if (LBA_string.size() > 2)
+		if (LBA_string.length() > 2)
 			return false;
 		bool flag = true;
-		for (int i = 0; i < LBA_string.size(); i++) {
+		for (int i = 0; i < LBA_string.length(); i++) {
 			if (LBA_string[i] < '0' || LBA_string[i] > '9') {
 				flag = false;
 				break;
@@ -62,6 +88,24 @@ public:
 	bool isLBAinRange(string check_lba) {
 		if ((0 <= stoi(check_lba)) && (stoi(check_lba) < 100))
 			return true;
+		return false;
+	}
+
+	bool isDataValid(string check_data) {
+		if (check_data.length() != 10)
+			return false;
+		if (check_data[0] == '0' && check_data[1] == 'x') {
+			bool flag = true;
+			for (int i = 2; i < 10; i++) {
+				if (('A' <= check_data[i] <= 'F') || ('0' <= check_data[i] <= '9'))	continue;
+				else {
+					flag = false;
+					break;
+				}
+			}
+			if (flag == true)
+				return true;
+		}
 		return false;
 	}
 };
--- TestShell/TestShell/main.cpp
+++ TestShell/TestShell/main.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 int main() {
 	string input_cmd;
+	TestShell ts;
 	CommandParser cp;
 	while (1) {
 		try {
@@ -19,8 +20,15 @@ int main() {
 
 		if (cp.cmd == "exit")
 			break;
-		else if (input_cmd == "help")
+		else if (cp.cmd == "help")
 			cout << "도움말 추가 예정\n";
-
+		else if (cp.cmd == "read")
+			ts.Read(cp.lba);
+		else if (cp.cmd == "write")
+			ts.Write(cp.lba, cp.data);
+		else if (cp.cmd == "fullwrite")
+			ts.FulllWrite(cp.data);
+		else if (cp.cmd == "fullread")
+			ts.FullRead();
 	}
 }
