fc2c521f91965e7c2f4be2bb031ca702f61a933f
yoyo <arkdusdyk@gmail.com>
2024-06-05 12:04:07 +0900

    Parser add 2 tokens (Read LBA)

--- TestShell/TestShell/CommandParser.cpp
+++ TestShell/TestShell/CommandParser.cpp
@@ -5,10 +5,16 @@ using namespace std;
 
 class CommandParser {
 public:
+	string cmd;
+	int lba;
+	string data;
+	
 	void command_parse(string cmd_line) {
+		if (cmd_line == "")
+			throw exception("No Command");
 		vector<string> tokens;
 		tokens.clear();
-		int prev;
+		int prev = 0;
 		int cur = cmd_line.find(' ');
 		while (cur != string::npos) {
 			string substring = cmd_line.substr(prev, cur - prev);
@@ -16,19 +22,46 @@ public:
 			prev = cur + 1;
 			cur = cmd_line.find(' ', prev);
 		}
-		if (tokens.empty())
-			throw exception("No Command");
-		else if (tokens.size() == 1)
+		tokens.push_back(cmd_line.substr(prev, cur - prev));
+
+		if (tokens.size() == 1)				//	exit, help, fullwrite, fullread
 			cmd = tokens[0];
-		else if (tokens.size() == 2)
-			cmd = tokens[1];
-		else if (tokens.size() == 3)
-			cmd = tokens[2];
-		else
-			throw exception("Check Argument");
+		else if (tokens.size() == 2) {		// read LBA
+			cmd = tokens[0];
+			if (isLBAint(tokens[1]) == false)
+				throw exception("LBA out of range");
+			if (isLBAinRange(tokens[1]) == false)
+				throw exception("LBA out of range");
+			lba = stoi(tokens[1]);
+		}
+		else if (tokens.size() == 3) {		// write LBA data
+			cmd = tokens[0];
+			if (isLBAint(tokens[1]) == false)
+				throw exception("LBA out of range");
+			if (isLBAinRange(tokens[1]) == false)
+				throw exception("LBA out of range");
+			lba = stoi(tokens[1]);
+		}
+		else if(tokens.size() > 3)
+			throw exception("INVALID COMMAND");
 	}
-private:
-	string cmd;
-	int lba;
-	string data;
+
+	bool isLBAint(string LBA_string) {
+		if (LBA_string.size() > 2)
+			return false;
+		bool flag = true;
+		for (int i = 0; i < LBA_string.size(); i++) {
+			if (LBA_string[i] < '0' || LBA_string[i] > '9') {
+				flag = false;
+				break;
+			}
+		}
+		return flag;
+	}
+
+	bool isLBAinRange(string check_lba) {
+		if ((0 <= stoi(check_lba)) && (stoi(check_lba) < 100))
+			return true;
+		return false;
+	}
 };
--- TestShell/TestShell/main.cpp
+++ TestShell/TestShell/main.cpp
@@ -1,16 +1,26 @@
+#include <stdexcept>
 #include <iostream>
 #include <string>
 #include "TestShell.cpp"
+#include "CommandParser.cpp"
 
 using namespace std;
 int main() {
 	string input_cmd;
+	CommandParser cp;
 	while (1) {
-		cin >> input_cmd;
-		if (input_cmd == "exit")
+		try {
+			getline(cin, input_cmd);
+			cp.command_parse(input_cmd);
+		}
+		catch (exception) {
+			cout << "INVALID COMMAND\n";
+		}
+
+		if (cp.cmd == "exit")
 			break;
 		else if (input_cmd == "help")
-			cout << "도움말 추가 예정";
+			cout << "도움말 추가 예정\n";
 
 	}
 }
