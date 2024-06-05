f288e1ded37a5b13c42d5d4193107f68f7989a95
yoyo <arkdusdyk@gmail.com>
2024-06-05 14:08:52 +0900

    Fix bug - exception 수정

--- TestShell/TestShell/main.cpp
+++ TestShell/TestShell/main.cpp
@@ -13,22 +13,21 @@ int main() {
 		try {
 			getline(cin, input_cmd);
 			cp.command_parse(input_cmd);
+			if (cp.cmd == "exit")
+				break;
+			else if (cp.cmd == "help")
+				cout << "도움말 추가 예정\n";
+			else if (cp.cmd == "read")
+				ts.Read(cp.lba);
+			else if (cp.cmd == "write")
+				ts.Write(cp.lba, cp.data);
+			else if (cp.cmd == "fullwrite")
+				ts.FulllWrite(cp.data);
+			else if (cp.cmd == "fullread")
+				ts.FullRead();
 		}
 		catch (exception) {
 			cout << "INVALID COMMAND\n";
 		}
-
-		if (cp.cmd == "exit")
-			break;
-		else if (cp.cmd == "help")
-			cout << "도움말 추가 예정\n";
-		else if (cp.cmd == "read")
-			ts.Read(cp.lba);
-		else if (cp.cmd == "write")
-			ts.Write(cp.lba, cp.data);
-		else if (cp.cmd == "fullwrite")
-			ts.FulllWrite(cp.data);
-		else if (cp.cmd == "fullread")
-			ts.FullRead();
 	}
 }
