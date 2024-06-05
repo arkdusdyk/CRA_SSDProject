1ad67f5e8a0632a7b52ea2e1d245908986654934
yoyo <arkdusdyk@gmail.com>
2024-06-05 16:30:50 +0900

    [Feature] Help 문구 수정

--- TestShell/TestShell/main.cpp
+++ TestShell/TestShell/main.cpp
@@ -20,7 +20,13 @@ int main() {
 			if (cp.cmd == "exit")
 				break;
 			else if (cp.cmd == "help")
-				cout << "도움말 추가 예정\n";
+				cout << "주요 명령어\n"
+				<< "1. write (예) write LBA data : LBA 에 data를 기록)\n"
+				<< "2. read (예) read LBA : LBA 를 읽음)\n"
+				<< "3. exit (예) exit : shell 을 종료)\n"
+				<< "4. help (예) help : 주요 명령어의 사용방법 출력)\n"
+				<< "5. fullwrite (예) fullwrite data : 모든 LBA(0~99)까지 data 를 write 수행)\n"
+				<< "6. fullread (예) fullread : 모든 LBA(0~99) 까지 read 수행)\n";
 			else if (cp.cmd == "read")
 				ts.Read(cp.lba);
 			else if (cp.cmd == "write")
