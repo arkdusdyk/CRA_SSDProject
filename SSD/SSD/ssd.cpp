2c955c99fcd0af181533d60a93dae79bbcdc582a
bsk <lightjd@naver.com>
2024-06-05 09:46:58 +0900

    Refactoring: Applied command pattern on ssd commands

--- SSD/SSD/ssd.cpp
+++ SSD/SSD/ssd.cpp
@@ -7,74 +7,11 @@
 
 using namespace std;
 
-struct CommandSet
-{
-	int cmdOpcode;
-	int address;
-	int data;
-};
 
 class SSD {
 public:
-	static const int COMMAND_VALIDATION_SUCCESS = 0;
-	static const int COMMAND_VALIDATION_FAIL = -1;
-
 	static const int COMMAND_WRITE = 0x1;
 	static const int COMMAND_READ = 0x2;
-
-	bool isDecimal(const std::string& str) {
-		if (str.empty()) return false;
-
-		for (char c : str) {
-			if (!(std::isdigit(c))) {
-				return false;
-			}
-		}
-
-		return true;
-	}
-
-	bool isHexadecimal(const std::string& str) {
-		if (str.empty()) return false;
-
-		for (char c : str) {
-			if (!(std::isdigit(c) || (std::isalpha(c) && std::toupper(c) >= 'A' && std::toupper(c) <= 'F'))) {
-				return false;
-			}
-		}
-
-		return true;
-	}
-
-	int checkParameter(int argc, char* argv[], CommandSet& commandset) {
-
-		if (argc >= 3 && !isDecimal(string(argv[2])))
-		{
-			return COMMAND_VALIDATION_FAIL;
-		}
-
-		if (argc >= 4 && !isHexadecimal(string(argv[3])))
-		{
-			return COMMAND_VALIDATION_FAIL;
-		}
-
-		if (argc == 4 && string(argv[1]) == "W")
-		{
-			commandset.cmdOpcode = SSD::COMMAND_WRITE;
-			commandset.address = std::stoi(string(argv[2]));
-			commandset.data = static_cast<int>(std::stoll(string(argv[3]), nullptr, 16));
-		}
-		else if (argc == 3 && string(argv[1]) == "R")
-		{
-			commandset.cmdOpcode = SSD::COMMAND_READ;
-			commandset.address = std::stoi(string(argv[2]));
-			commandset.data = -1;
-		}
-		else
-			return COMMAND_VALIDATION_FAIL;
-
-		return COMMAND_VALIDATION_SUCCESS;
-	}
 
 	void write(int address, int data) {
 		string ssdData[100];
