4e17259fc70172633465f91791dbb798f9f8ab5e
kwonluck <software_k@naver.com>
2024-06-07 15:11:27 +0900

    [FEAT] Logger API관련 매개변수 추가
    1. 매개변수로 Console 출력 여부 지정
    2. 현재 시간 및 함수명 처리
    3. Log 파일 생성시 dir 검사 후 진행.

--- TestShell/LoggerLib/Logger.cpp
+++ TestShell/LoggerLib/Logger.cpp
@@ -1,11 +1,78 @@
 #include "pch.h"
 #include "Logger.h"
-#include "iostream"
 
-void Logger::write_Log(string log) {
+#include<iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <chrono>
+#include <filesystem>
+
+void Logger::write_Log(eLoggingOpt loggingOption, string sfunctionName, string log_detail) {
+	string log = getCurrentTimetoString()+ " " + setPaddingString(sfunctionName+"()") + " : " + log_detail;
+
+	switch (loggingOption)
+	{
+	case eLoggingOpt::ALL_PRINT:
+		printConsole(log);
+		printFile(log);
+		break;
+	case eLoggingOpt::ONLY_FILE:
+		printFile(log);
+		break;
+	case eLoggingOpt::ONLY_CONSOLE:
+		printConsole(log);
+		break;
+	default:
+		break;
+	}
+}
+
+
+string Logger::getCurrentTimetoString() {
+	auto chrono_Now = std::chrono::system_clock::now();
+	auto millisec = chrono::duration_cast<chrono::milliseconds>(chrono_Now.time_since_epoch()) % 1000;
+
+	time_t tm_now = chrono::system_clock::to_time_t(chrono_Now);
+	tm* now_time = new tm();
+	localtime_s(now_time, &tm_now);
+
+	char temp[128];
+	strftime(temp, sizeof(temp), "%Y-%m-%d %H:%M:%S.", now_time);
+	
+	std::ostringstream oss;
+	oss << "[" << temp << left << setfill('0') << setw(3) << millisec.count() << "]";
+
+	return oss.str();
+}
+
+string Logger::setPaddingString(const string& str)
+{
+	std::ostringstream oss;
+	oss << left << setfill(' ') << setw(30) << str;
+	return oss.str();
+}
+
+void Logger::printConsole(const string& log) {
 	cout << log << endl;
 }
 
-void write_Log(Logger* looger, string log) {
-	looger->write_Log(log);
+void Logger::printFile(const string& log) {
+
+	// dir 존재 여부 체크
+	filesystem::path dirPath("log");
+	if (filesystem::exists(dirPath) == false) {
+		if (create_directory(dirPath)) {
+			cout << "Directory created successfully." << endl;
+		}
+	}
+
+	// 파일 크기 체크 및 변환 구현 필요.
+
+	ofstream logFile("log\\latest.log", ios::app);
+	if (logFile.is_open()) {
+		logFile << log << endl;
+	}
+
+	logFile.close();
 }
